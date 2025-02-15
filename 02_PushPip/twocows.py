96b6b3e5ba26ff75e68a33535ef625a4b7c0a113
ArtyomOsipchuk <artos4002@yandex.ru>
2025-02-15 23:58:44 +0300

    Added second cow

--- 02_PushPip/twocows.py
+++ 02_PushPip/twocows.py
@@ -3,10 +3,11 @@ import cowsay
 
 parser = argparse.ArgumentParser(
                     prog='Twocows',
-                    description='Cowsay with 1 cow',
+                    description='Cowsay with 2 cows',
                     epilog='Cows is cool.')
 
-parser.add_argument('message') # positional argument
+parser.add_argument('message1') # positional argument
+parser.add_argument('message2') # positional argument
 parser.add_argument('-e') #eye_string
 parser.add_argument('-f') #cowfile
 parser.add_argument('-n', action="store_true") 
@@ -39,10 +40,27 @@ if args.w:
     pr += "w"
 if args.y:
     pr += "y"
-print(cowsay.cowsay(args.message1,
+cow1 = cowsay.cowsay(args.message1,
     cow=args.f if args.f else 'default',
     preset=pr, eyes=args.e if args.e else 'oo',
     tongue=args.T if args.T else '  ',
     width=int(args.W) if args.W else 40,
-    wrap_text=args.n,
-))
+    wrap_text=not args.n,
+).split("\n")
+cow2 = cowsay.cowsay(args.message2,
+    cow=args.F if args.F else 'default',
+    preset=pr, eyes=args.E if args.E else 'oo',
+    tongue=args.T if args.T else '  ',
+    width=int(args.W) if args.W else 40,
+    wrap_text=not args.N,
+).split('\n')
+wid = max(len(i) for i in cow1 + cow2)
+sh = len(cow1) - len(cow2)
+if sh > 0:
+    for i in range(sh):
+        cow2.insert(0, ' ' * wid)
+elif sh < 0:
+    for i in range(-sh):
+        cow1.insert(0, ' ' * wid)
+for i in range(len(cow1)):
+    print(cow1[i].ljust(wid) + cow2[i])
