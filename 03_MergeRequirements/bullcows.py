ca3f1b77a63f5eabe11ae570648746a966f71f68
ArtyomOsipchuk <artos4002@yandex.ru>
2025-02-27 20:10:24 +0300

    Добавлена функция-приложение, обеспечивающая геймплей. -gameplay приложение -ask спрашивает, ограничивает ввод -inform печатает

--- 03_MergeRequirements/bullcows.py
+++ 03_MergeRequirements/bullcows.py
@@ -1,4 +1,6 @@
-def bullscows(answer: str, jigsaw: str):
+import random
+
+def bullscows(answer: str, jigsaw: str) -> (int, int):
     b, c = 0, 0
     an = list(answer)
     jig = list(jigsaw)
@@ -7,8 +9,33 @@ def bullscows(answer: str, jigsaw: str):
             jig = jig[:i - b] + jig[i+1 - b:]
             an = an[:i - b] + an[i+1 - b:]
             b += 1
-    for i in an: #set?
+    for i in an:
         if i in jig:
             jig.remove(i)
             c += 1
     return b, c
+
+def ask(prompt: str, valid: list[str] = None) -> str:
+    answer = input(prompt)
+    if valid:
+        while answer not in valid:
+            answer = input(prompt)
+    return answer
+
+def inform(format_string: str, bulls: int, cows: int) -> None:
+    print(format_string.format(bulls, cows))
+
+def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
+    jigsaw = random.choice(words)
+    tries = 0
+    answer = 'not' + jigsaw
+    while answer != jigsaw:
+        tries += 1
+        answer = ask("Введите слово: ", words)
+        b, c = bullscows(answer, jigsaw)
+        inform("Быки: {}, Коровы: {}", b, c)
+    ask(f"🏆Победа-победа вместо обеда!🏆\n{tries} попыток.\nНажмите любую кнопку для выхода...")
+    return tries
+
+if __name__ == "__main__":
+    gameplay(ask, inform, ["йожик", "котик", "собак", "уттка"])
